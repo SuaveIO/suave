1f1a30807657ffbe9e5ffdd5c51286555d6d11c7
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2013-09-16 21:50:19 -0400

    this doesn't work well in f#

--- Suave/Log.fs
+++ Suave/Log.fs
@@ -6,5 +6,4 @@ open System.Diagnostics
 let console (s:string) =  Console.Write(s);
 
 let log format = 
-    let callr = (new StackFrame(1)).GetMethod().Name 
-    (Printf.kprintf (printf "%A: %s - %s" DateTime.Now callr) format) 
+    (Printf.kprintf (printf "%A: %s" DateTime.Now) format) 
