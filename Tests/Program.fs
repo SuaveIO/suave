0e67ce5418f2a46b42629fe92ef30594bad51225
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2014-02-05 17:39:02 -0500

    a pair of simple cookie tests

--- Tests/Program.fs
+++ Tests/Program.fs
@@ -182,20 +182,39 @@ let posts =
 let cookies =
   let run_with' = run_with default_config
 
+  let basic_cookie = 
+    { name      = "mycookie"
+    ; value     = "42"
+    ; expires   = None
+    ; domain    = None
+    ; path      = Some "/"
+    ; http_only = false
+    ; secure    = false
+    ; version   = None 
+    }
+
   testList "Cookies basic tests"
     [
       testCase "cookie data makes round trip" <| fun _ ->
         Assert.Equal("expecting cookie value"
         , "42"
         , (req_cookies GET "/" None
-          (run_with' (set_cookie { name = "mycookie"
-            ; value = "42"
-            ; expires = None
-            ; domain = None
-            ; path = Some "/"
-            ; http_only = false
-            ; secure = false
-            ; version = None} >> succeed >>= OK "test"))).GetCookies(Uri("http://127.0.0.1")).[0].Value)
+          (run_with' (set_cookie basic_cookie >> succeed >>= OK "test")))
+            .GetCookies(Uri("http://127.0.0.1")).[0].Value)
+
+      testCase "cookie name makes round trip" <| fun _ ->
+        Assert.Equal("expecting cookie name"
+        , "mycookie"
+        , (req_cookies GET "/" None
+          (run_with' (set_cookie basic_cookie >> succeed >>= OK "test")))
+            .GetCookies(Uri("http://127.0.0.1")).[0].Name)
+
+      testCase "http_only cookie is http_only" <| fun _ ->
+        Assert.Equal("expecting http_only"
+        , true
+        , (req_cookies GET "/" None
+          (run_with' (set_cookie { basic_cookie with http_only = true } >> succeed >>= OK "test")))
+            .GetCookies(Uri("http://127.0.0.1")).[0].HttpOnly)
     ]
 
 open OpenSSL.X509
