40efd2327ece995ef772e22ce9f51203cd2c9250
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2014-02-09 19:07:04 -0500

    released v0.2.0

--- Suave/AssemblyInfo.fs
+++ Suave/AssemblyInfo.fs
@@ -2,7 +2,7 @@ namespace Suave
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
-[<assembly: AssemblyVersion("v0.0.5")>]
+[<assembly: AssemblyVersion("v0.2.0")>]
 [<assembly: AssemblyProduct("Suave.IO")>]
 [<assembly: AssemblyTitle("Suave.IO Framework")>]
 [<assembly: AssemblyDescription("Suave is an F# library providing a lightweight web server and a set of combinators to manipulate route flow and task composition.")>]
