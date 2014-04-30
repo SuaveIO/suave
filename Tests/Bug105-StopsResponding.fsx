d012fd68f52acf50f184fc4fb41700a96490a092
Henrik <henrik@haf.se>
2014-04-30 16:35:46 +0200

    creating repro, /cc @ademar relevant to #105 and possibly to #106 after #105 is solved

---
+++ Tests/Bug105-StopsResponding.fsx
@@ -0,0 +1,93 @@
+﻿#r "bin/Release/suave.dll"
+#r "System.Net"
+#r "System.Net.Http"
+#r "System.Xml"
+#r "System.Xml.Linq"
+
+module XD =
+  open System.IO
+  open System.Xml
+  open System.Xml.Linq
+  let mk_xdoc raw_bytes =
+    use xml_stream = new MemoryStream(raw_bytes : byte [])
+    use rdr = new XmlTextReader(xml_stream)
+    XDocument.Load rdr
+
+module SystemUnderTest =
+  open Suave
+  open Suave.Http
+  open Suave.Http.Successful
+  open Suave.Http.RequestErrors
+  open Suave.Http.ServerErrors
+  open Suave.Http.Applicatives
+  open Suave.Types
+  open Suave.Web
+
+  let app =
+    choose [
+      url "/deserialise_xml"
+        >>= request (fun r -> r.raw_form |> XD.mk_xdoc |> ignore
+                              ACCEPTED ".")
+      NOT_FOUND "bad route" ]
+
+  let conf =
+    { Web.default_config with
+        error_handler =
+          (fun ex reason ->
+            printfn "error: %O" ex
+            INTERNAL_ERROR "Failed test" >> (fun opt -> opt |> Option.get)) }
+
+  let run () =
+    let ready, listens =
+      Web.web_server_async conf app
+    listens |> Async.Start
+    ready |> Async.RunSynchronously |> ignore
+
+module Client =
+  open System
+  open System.IO
+  open System.Net.Http
+  open System.Threading
+
+  let client_post (uri : Uri) (ct : CancellationToken) (client : HttpClient) (data : 'a) =
+    let r = new HttpRequestMessage(HttpMethod.Post, uri)
+    r.Headers.ConnectionClose <- Nullable(true)
+    client.PostAsync(uri, data, ct) |> Async.AwaitTask
+
+  let uri str =
+    Uri str
+
+  let with_client f =
+    use cts = new CancellationTokenSource()
+    use handler = new Net.Http.HttpClientHandler(AllowAutoRedirect = true)
+    use client = new Net.Http.HttpClient(handler)
+    f cts handler client
+
+  let post data =
+    with_client <| fun cts handler client ->
+      new ByteArrayContent(data)
+      |> client_post (uri "http://localhost:8083/deserialise_xml") (cts.Token) client
+      |> Async.RunSynchronously
+
+  let post_and_assert =
+    post
+    >> fun (m : HttpResponseMessage) ->
+          let res = (m.Content.ReadAsStringAsync() |> Async.AwaitTask |> Async.RunSynchronously)
+          m.EnsureSuccessStatusCode() |> ignore
+          printf "%s" res
+
+  let ad = @"large_xml.xml" |> File.ReadAllBytes
+
+  let run () =
+    for i in 1 .. 500000 do
+      if i % 1000 = 0 then printfn "at %i" i
+      try
+        post_and_assert ad
+      with :? System.AggregateException as e when e.ToString().Contains("The underlying connection was closed") ->
+        printfn "%O" e
+
+// run test
+SystemUnderTest.run (); Client.run()
+
+// now if the test stopped, try:
+Client.post_and_assert Client.ad
