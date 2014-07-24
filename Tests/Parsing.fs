879d32a917733c9a4035b08f14f9853f53a2b511
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2014-07-24 19:04:49 -0400

    better POST data parsing test

--- Tests/Parsing.fs
+++ Tests/Parsing.fs
@@ -5,6 +5,7 @@ open Fuchu
 open System
 open System.IO
 open System.Net.Http
+open System.Net.Http.Headers
 open System.Text
 
 open Suave
@@ -27,12 +28,21 @@ let parsing_multipart =
       | Some str -> OK str
       | None -> OK "FAILED")
 
+  let test_multipart_form = 
+    request(fun r -> 
+      match get_first r.multipart_fields "From" with
+      | Some str -> OK str
+      | None -> OK "FAILED")
+
+  let byte_array_content = new ByteArrayContent(post_data1)
+  byte_array_content.Headers.TryAddWithoutValidation("Content-Type","multipart/form-data; boundary=99233d57-854a-4b17-905b-ae37970e8a39") |> ignore
+
   testList "http parser tests" [
       testCase "parsing a large multipart form" <| fun _ ->
-        Assert.Equal("expecting 'Pass'", "Pass", run_with' (OK "Pass") |> req_gzip POST "/" (Some <| new ByteArrayContent(post_data1))) 
+        Assert.Equal("", "Bob <bob@wishfulcoding.mailgun.org>", run_with' test_multipart_form |> req POST "/" (Some <| byte_array_content))
 
       testCase "parsing a large urlencoded form data" <| fun _ ->
-        Assert.Equal("expecting 'Pass'", "hallo wereld", 
+        Assert.Equal("", "hallo wereld", 
           run_with' test_url_encoded_form |> req_gzip POST "/" (Some <| new StringContent(post_data2, Encoding.UTF8, "application/x-www-form-urlencoded")))
 
   ]
