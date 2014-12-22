435908c43497d4014f9de523f20ba10099fbd05c
Justin <justin@agnomendesign.com>
2014-12-22 23:36:10 +0200

    intial commit of default assembly info

---
+++ Tests/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace Suave.Tests
+
+open System.Reflection
+open System.Resources;
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Suave Unit Tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Suave.io")>]
+[<assembly: AssemblyProduct("Suave.io")>]
+[<assembly: AssemblyCopyright("Copyright (c) Suave.io")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: NeutralResourcesLanguage("en")>]
+[<assembly: AssemblyVersion("1.0")>]
+[<assembly: AssemblyFileVersion("1.0")>]
+[<assembly: AssemblyInformationalVersion("1.0")>]
+[<assembly: ComVisible(false)>]
+()
