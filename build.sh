838da25de95e999c2dfe1cded270a466e1999b7f
Henrik Feldt <henrik@haf.se>
2014-12-22 15:53:44 +0100

    simplify build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-FOLDER=./packages/NuGet.CommandLine/tools
-mkdir -p $FOLDER
-
-NUGET=$FOLDER/NuGet.exe
-
-if [ ! -f $NUGET ]; then
-  curl -L -o $NUGET https://www.nuget.org/nuget.exe
-  chmod a+x $NUGET
-fi
-
 echo 'Restoring nugets'
 mono tools/paket.bootstrapper.exe
 mono tools/paket.exe restore
