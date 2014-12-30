7d8e53a0a788e9031daa3c58ca829dd69f3bd007
Justin <justin@agnomendesign.com>
2014-12-30 22:40:55 +0200

    WIP: sharding Utils into multiple module files

---
+++ Suave/utils/ASCII.fs
@@ -0,0 +1,28 @@
+﻿namespace Suave.Utils
+
+[<RequireQualifiedAccess>]
+module ASCII =
+  open System
+  open System.Text
+
+  /// Get the ASCII bytes for the string
+  let inline bytes (s : string) =
+    Encoding.ASCII.GetBytes s
+
+  /// Convert the byte array of ASCII-encoded chars to a string, starting at 'index' for 'count' characters
+  /// (each character is necessarily one byte)
+  let inline to_string (buff : byte[]) (index : int) (count : int) =
+    Encoding.ASCII.GetString(buff, index, count)
+
+  let inline to_string' (b : byte []) =
+    Encoding.ASCII.GetString b
+
+  /// Encode the string as ASCII encoded in Base64.
+  let inline base64_encode (s : string) =
+    let bytes = Encoding.ASCII.GetBytes s
+    Convert.ToBase64String bytes
+
+  /// Decode the string containing Base64-encoded ASCII string data to a string
+  let inline base64_decode (s : string) =
+    let bytes = Convert.FromBase64String s
+    Encoding.ASCII.GetString bytes
