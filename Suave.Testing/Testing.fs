85e712cf63d76b855569922d40841383ed2ac5b5
Henrik Feldt <henrik@haf.se>
2015-01-02 12:52:34 +0100

    [testing] cleaning up Post vs Send code, documentation

--- Suave.Testing/Testing.fs
+++ Suave.Testing/Testing.fs
@@ -1,15 +1,16 @@
-﻿module Suave.Testing
-
-(** For testing suave applications easily
+﻿(** For testing suave applications easily
 
 Example:
 
   open Suave
+  open Suave.Web
   open Suave.Types
   open Suave.Testing
 
   open Fuchu
 
+  let run_with' = run_with default_config
+  
   testCase "parsing a large multipart form" <| fun _ ->
 
     let res =
@@ -19,6 +20,7 @@ Example:
     Assert.Equal("", "Bob <bob@wishfulcoding.mailgun.org>", )
 
 *)
+module Suave.Testing
 
 open System
 open System.Threading
@@ -49,14 +51,52 @@ module ResponseData =
   let content_byte_array (response : HttpResponseMessage) =
     response.Content.ReadAsByteArrayAsync().Result
 
+module Utilities =
+    
+  /// Utility function for mapping from Suave.Types.HttpMethod to
+  /// System.Net.Http.HttpMethod.
+  let to_http_method = function
+    | HttpMethod.GET -> HttpMethod.Get
+    | HttpMethod.POST -> HttpMethod.Post
+    | HttpMethod.DELETE -> HttpMethod.Delete
+    | HttpMethod.PUT-> HttpMethod.Put
+    | HttpMethod.HEAD -> HttpMethod.Head
+    | HttpMethod.TRACE -> HttpMethod.Trace
+    | HttpMethod.OPTIONS -> HttpMethod.Options
+    | HttpMethod.PATCH -> failwithf "PATCH not a supported method in HttpClient"
+    | HttpMethod.CONNECT -> failwithf "CONNECT not a supported method in the unit tests"
+    | HttpMethod.OTHER x -> failwithf "%A not a supported method" x
+
+
+open Utilities
+
+/// This test context is a holder for the runtime values of the web
+/// server of suave, as well as the cancellation token that is
+/// threaded throughout the web server and will shut down all
+/// concurrently running async operations.
+///
+/// When you are done with it, you should call `dispose_context` to
+/// cancel the token and dispose the server's runtime artifacts
+/// (like the listening socket etc).
 type SuaveTestCtx =
   { cts          : CancellationTokenSource
     suave_config : SuaveConfig }
 
+/// Cancels the cancellation token source and disposes the server's
+/// resources.
 let dispose_context (ctx : SuaveTestCtx) =
   ctx.cts.Cancel()
   ctx.cts.Dispose()
 
+/// Create a new test context from a factory that starts the web
+/// server, such as `web_server_async` from `Suave.Web`. Also pass
+/// in a `SuaveConfig` value and the web parts you'd like to test.
+///
+/// The factory needs to start two async's, one which this function
+/// can block on (listening) and another (server) which is the actual
+/// async value of the running server. The listening async value will
+/// be awaited inside this function but the server async value will
+/// be run on the thread pool.
 let run_with_factory factory config web_parts : SuaveTestCtx =
   let binding = config.bindings.Head
   let base_uri = binding.ToString()
@@ -70,33 +110,37 @@ let run_with_factory factory config web_parts : SuaveTestCtx =
   { cts = cts
     suave_config = config' }
 
+/// Similar to run_with_factory, but uses the default suave factory.
 let run_with = run_with_factory web_server_async
 
-let to_http_method = function
-  | HttpMethod.GET -> HttpMethod.Get
-  | HttpMethod.POST -> HttpMethod.Post
-  | HttpMethod.DELETE -> HttpMethod.Delete
-  | HttpMethod.PUT-> HttpMethod.Put
-  | HttpMethod.HEAD -> HttpMethod.Head
-  | HttpMethod.TRACE -> HttpMethod.Trace
-  | HttpMethod.OPTIONS -> HttpMethod.Options
-  | HttpMethod.PATCH -> failwithf "PATCH not a supported method in HttpClient"
-  | HttpMethod.CONNECT -> failwithf "CONNECT not a supported method in the unit tests"
-  | HttpMethod.OTHER x -> failwithf "%A not a supported method" x
-
-/// ensures the context is disposed after 'f ctx' is called
+/// Ensures the context is disposed after 'f ctx' is called.
 let with_context f ctx =
   try
     f ctx
   finally dispose_context ctx
 
+/// This is the main function for the testing library; it lets you assert
+/// on the request/response values while ensuring deterministic
+/// disposal of suave.
+///
+/// Currently, it:
+///
+///  - doesn't automatically follow 301 FOUND redirects (nor 302, 307) to
+///    ensure you can assert on redirects.
+///  - only requests to the very first binding your web server has in use
+///  - only sets a HttpContent if you have given a value to the `data`
+///    parameter.
+///  - waits 5000 ms for a reply, then breaks into the debugger if you're
+///    attached, otherwise asserts a failure of the timeout
+///  - calls `f_result` with the HttpResponseMessage
+///
 let req_resp
   (methd : HttpMethod)
   (resource : string)
   (query : string)
   data
-  (cookies : Net.CookieContainer option)
-  (decompressionMethod : Net.DecompressionMethods)
+  (cookies : CookieContainer option)
+  (decompressionMethod : DecompressionMethods)
   f_result =
 
   with_context <| fun ctx ->
@@ -113,13 +157,9 @@ let req_resp
 
     let r = new HttpRequestMessage(to_http_method methd, uri_builder.Uri)
     r.Headers.ConnectionClose <- Nullable(true)
+    data |> Option.iter (fun data -> r.Content <- data)
 
-    let get =
-      match data with
-      | Some data ->
-        client.PostAsync(uri_builder.Uri, data, ctx.cts.Token)
-      | None ->
-        client.SendAsync(r, HttpCompletionOption.ResponseContentRead, ctx.cts.Token)
+    let get = client.SendAsync(r, HttpCompletionOption.ResponseContentRead, ctx.cts.Token)
 
     let completed = get.Wait(5000)
     if not completed && System.Diagnostics.Debugger.IsAttached then System.Diagnostics.Debugger.Break()
@@ -155,6 +195,7 @@ let req_headers methd resource data =
 let req_content_headers methd resource data =
   req_resp methd resource "" data None DecompressionMethods.None content_headers
 
+/// Test a request by looking at the cookies alone.
 let req_cookies methd resource data ctx =
   let cookies = new CookieContainer()
   req_resp
