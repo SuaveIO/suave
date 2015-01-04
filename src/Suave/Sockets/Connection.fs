dd5615388946b6048d344700cb95ec0294289db8
Henrik Feldt <henrik@haf.se>
2015-01-04 15:35:34 +0100

    Connection

--- src/Suave/Sockets/Connection.fs
+++ src/Suave/Sockets/Connection.fs
@@ -15,6 +15,7 @@ type Connection =
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Connection =
+
   let empty : Connection =
     { ipaddr     = IPAddress.Loopback
       transport       =  { socket = null; read_args = null; write_args   = null }
@@ -27,3 +28,23 @@ module Connection =
   
   let inline send (cn :Connection) (buf : ByteSegment) =
     cn.transport.write buf
+
+  let ipaddr_ =
+    (fun x -> x.ipaddr),
+    fun v x -> { x with ipaddr = v }
+
+  let transport_ =
+    (fun x -> x.transport),
+    fun v x -> { x with transport = v }
+
+  let buffer_manager_ =
+    (fun x -> x.buffer_manager),
+    fun v x -> { x with buffer_manager = v }
+
+  let line_buffer_ =
+    (fun x -> x.line_buffer),
+    fun v x -> { x with line_buffer = v }
+
+  let segments_ =
+    (fun x -> x.segments),
+    fun v x -> { x with segments = v }
