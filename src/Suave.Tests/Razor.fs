d40d097e834ec4ed5f85fd03df507a440286ecfd
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2015-06-12 17:12:34 -0400

    simple unit test for the razor module

---
+++ src/Suave.Tests/Razor.fs
@@ -0,0 +1,31 @@
+﻿module Suave.Tests.Razor
+
+open Fuchu
+
+open System
+open System.IO
+open System.Net.Http
+open System.Net.Http.Headers
+open System.Text
+
+open Suave
+open Suave.Types
+open Suave.Http
+open Suave.Razor
+
+open Suave.Tests.TestUtilities
+open Suave.Testing
+
+type Foo = { bar : string }
+
+[<Tests>]
+let razor_test =
+  let runWithConfig = runWith defaultConfig
+
+  testList "razor test" [
+    testCase "simple test" <| fun _ ->
+      Assert.Equal("process razor file", "Hello Foo", 
+        runWithConfig (razor<Foo> "razor.cshtml" { bar = "Foo" }) |> req HttpMethod.GET "/" None) ]
+
+
+
