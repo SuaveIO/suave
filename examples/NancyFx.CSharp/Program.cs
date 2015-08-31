57c125b26f528645e998b150b137c0c5f45562d4
Henrik Feldt <henrik@haf.se>
2015-08-31 10:52:54 +0200

    [owin] NancyFx C# example

---
+++ examples/NancyFx.CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Suave;
+using Nancy;
+using Nancy.Owin;
+
+namespace Suave.NancyFx
+{
+    public class SampleModule : NancyModule
+    {
+        public SampleModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+    
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var opts = new NancyOptions();
+            var app = Suave.Owin.OwinAppModule.OfMidFunc(NancyMiddleware.UseNancy(opts));
+            Web.startWebServer(Web.defaultConfig, app);
+            return 0;
+        }
+    }
+}
