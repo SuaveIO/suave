521e05807ca83a8dbacef01c8d5441f2fd928a10
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2015-10-08 14:47:55 -0400

    Libuv module

--- src/Suave.LibUv/Tcp.fs
+++ src/Suave.LibUv/Tcp.fs
@@ -1,192 +1,11 @@
-
-module Suave.Native.LibUv
+﻿module Suave.LibUv.Tcp
 
 #nowarn "9"
 
 open System
 open System.Runtime.InteropServices
 
-let UV_RUN_DEFAULT = 0
-let UV_RUN_ONCE = 1
-let UV_RUN_NOWAIT = 2
-
-[<StructLayout(LayoutKind.Sequential)>]
-type sockaddr =
-  struct
-    val mutable sin_family : int16
-    val mutable sin_port : uint16
-  end
-
-type sockaddr_in =
-  struct
-    val mutable a : int64
-    val mutable b : int64
-    val mutable c : int64
-    val mutable d : int64
-  end
-
-[<StructLayout(LayoutKind.Sequential, Size=28)>]
-type sockaddr_in6 =
-  struct
-    val mutable a : int
-    val mutable b : int
-    val mutable c : int 
-    val mutable d : int
-    val mutable e : int
-    val mutable f : int
-    val mutable g : int
-  end
-
-[<StructLayout(LayoutKind.Sequential)>]
-type uv_buf_t =
-  struct
-    val mutable ``base`` : IntPtr // pointer to bytes
-    val mutable len  : int
-  end
-
-[<UnmanagedFunctionPointer(CallingConvention.Cdecl)>]
-type uv_connection_cb = delegate of IntPtr * int -> unit
-
-[<UnmanagedFunctionPointer(CallingConvention.Cdecl)>]
-type uv_alloc_cb = delegate of IntPtr * int * [<Out>] buf:byref<uv_buf_t> -> unit
-
-[<UnmanagedFunctionPointer(CallingConvention.Cdecl)>]
-type uv_read_cb  = delegate of IntPtr * int * byref<uv_buf_t> -> unit
-
-[<UnmanagedFunctionPointer(CallingConvention.Cdecl)>]
-type uv_write_cb  = delegate of IntPtr * int -> unit
-
-[<UnmanagedFunctionPointer(CallingConvention.Cdecl)>]
-type uv_close_cb  = delegate of IntPtr -> unit
-
-[<UnmanagedFunctionPointer(CallingConvention.Cdecl)>]
-type uv_handle_cb  = delegate of IntPtr -> unit
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_tcp_init(IntPtr loop, IntPtr handle)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_ip4_addr(string ip, int port, [<Out>] sockaddr_in& address)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_ip6_addr(string ip, int port, [<Out>] sockaddr_in6& address)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_tcp_bind(IntPtr handle, sockaddr_in& sockaddr, int flags)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_tcp_bind6(IntPtr handle, sockaddr_in6&  sockaddr, uint32 flags)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_listen(IntPtr stream, int backlog, uv_connection_cb callback)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_accept(IntPtr server, IntPtr client)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_read_start(IntPtr stream, uv_alloc_cb alloc_callback, uv_read_cb read_callback)
-
-[<DllImport ("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_read_stop(IntPtr stream)
-
-/// Loops
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern IntPtr uv_default_loop()
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_loop_init(IntPtr handle)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_loop_close(IntPtr ptr)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_loop_size()
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern void uv_stop(IntPtr loop)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_run(IntPtr loop, int mode)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern void uv_update_time(IntPtr loop)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern uint64 uv_now(IntPtr loop)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern sbyte *uv_strerror(int systemErrorCode)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern sbyte *uv_err_name(int systemErrorCode)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern void uv_close(IntPtr handle, uv_close_cb cb)
-
-[<DllImport("libuv.dll", EntryPoint = "uv_write", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_write(IntPtr req, IntPtr handle, uv_buf_t[] bufs, int bufcnt, uv_write_cb write_callback)
-//extern int uv_write(IntPtr req, IntPtr handle, uv_buf_t* bufs, int bufcnt, uv_write_cb write_callback)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_idle_init(IntPtr loop, IntPtr idle)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_idle_start(IntPtr idle, uv_handle_cb callback)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_idle_stop(IntPtr idle)
-
-type uv_handle_type =
-  | UV_UNKNOWN_HANDLE = 0
-  | UV_ASYNC          = 1
-  | UV_CHECK          = 2
-  | UV_FS_EVENT       = 3
-  | UV_FS_POLL        = 4
-  | UV_HANDLE         = 5
-  | UV_IDLE           = 6
-  | UV_NAMED_PIPE     = 7
-  | UV_POLL           = 8
-  | UV_PREPARE        = 9
-  | UV_PROCESS        = 10
-  | UV_STREAM         = 11
-  | UV_TCP            = 12
-  | UV_TIMER          = 13
-  | UV_TTY            = 14
-  | UV_UDP            = 15
-  | UV_SIGNAL         = 16
-  | UV_FILE           = 17
-  | UV_HANDLE_TYPE_PRIVATE = 18
-  | UV_HANDLE_TYPE_MAX     = 19
-
-type uv_request_type =
-  | UV_UNKNOWN_REQ = 0
-  | UV_REQ         = 1
-  | UV_CONNECT     = 2
-  | UV_WRITE       = 3
-  | UV_SHUTDOWN    = 4
-  | UV_UDP_SEND    = 5
-  | UV_FS          = 6
-  | UV_WORK        = 7
-  | UV_GETADDRINFO = 8
-  | UV_GETNAMEINFO = 9
-  | UV_REQ_TYPE_PRIVATE = 10
-  | UV_REQ_TYPE_MAX     = 11
-
-let UV_EOF = -4095
-let UV_ECONNRESET = -4077
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_handle_size(uv_handle_type t)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_req_size(uv_request_type t)
-
-[<DllImport("libuv.dll", CallingConvention=CallingConvention.Cdecl)>]
-extern int uv_async_init(IntPtr loop, IntPtr handle, uv_handle_cb callback)
-
-[<DllImport("libuv.dll", CallingConvention = CallingConvention.Cdecl)>]
-extern int uv_async_send(IntPtr handle);
+open Native
 
 type Handle = IntPtr
 
@@ -236,16 +55,16 @@ type ReadOp() =
   [<DefaultValue>] val mutable uv_alloc_cb : uv_alloc_cb
   [<DefaultValue>] val mutable uv_read_cb : uv_read_cb
 
-  member this.end_read' ((client : IntPtr), (nread : int), (buff : byref<uv_buf_t>)) =
+  member this.end_read ((client : IntPtr), (nread : int), (buff : byref<uv_buf_t>)) =
 
     let h = uv_read_stop(client)
 
     if (nread < 0) then
       if (nread <> UV_EOF) then
         let err = new string(uv_err_name(nread))
-        this.ok (Choice2Of2 <| LibUvError err)
+        this.ok (Choice2Of2 <| ConnectionError err)
       else
-        this.ok (Choice2Of2 <| LibUvError("eof"))
+        this.ok (Choice2Of2 <| ConnectionError("eof"))
     elif (nread > 0) then
       this.ok (Choice1Of2 nread)
 
@@ -253,14 +72,14 @@ type ReadOp() =
     buff.``base`` <- Marshal.UnsafeAddrOfPinnedArrayElement(this.buf.Array,this.buf.Offset)
     buff.len <- this.buf.Count
 
-  member this.uv_read_start'' cn buf ok =
+  member this.uv_read_start cn buf ok =
     this.buf <- buf
     this.ok <- ok
     uv_read_start(cn, this.uv_alloc_cb, this.uv_read_cb) |> ignore
 
   member this.Initialize() =
     this.uv_alloc_cb <- uv_alloc_cb(fun a b c -> this.alloc_buffer(a, b, &c))
-    this.uv_read_cb <- uv_read_cb(fun a b c -> this.end_read' (a, b, &c))
+    this.uv_read_cb <- uv_read_cb(fun a b c -> this.end_read (a, b, &c))
 
 type WriteOp() =
 
@@ -272,7 +91,7 @@ type WriteOp() =
     Marshal.FreeCoTaskMem request
     if (status < 0) then 
       let err = new string(uv_strerror(status))
-      this.ok (Choice2Of2 <| LibUvError err)
+      this.ok (Choice2Of2 <| ConnectionError err)
     else
       this.ok (Choice1Of2())
 
@@ -280,7 +99,7 @@ type WriteOp() =
     this.wrbuffArray <- [| new uv_buf_t() |]
     this.uv_write_cb <- uv_write_cb(this.end_write)
 
-  member this.uv_write'' cn (buf : ArraySegment<byte>) ok =
+  member this.uv_write cn (buf : ArraySegment<byte>) ok =
     this.ok <- ok
     this.wrbuffArray.[0].``base`` <- Marshal.UnsafeAddrOfPinnedArrayElement(buf.Array,buf.Offset)
     this.wrbuffArray.[0].len <- buf.Count
@@ -311,7 +130,7 @@ type LibUvTransport(pool : ConcurrentPool<OperationPair>,loop : IntPtr,client : 
     closeEvent.Set() |> ignore
     this.cont ()
 
-  member this.uv_close' cont =
+  member this.uv_close cont =
     this.cont <- cont
     intern "Calling uv_close."
     uv_close(client, this.uv_close_cb) |> ignore
@@ -322,18 +141,18 @@ type LibUvTransport(pool : ConcurrentPool<OperationPair>,loop : IntPtr,client : 
 
   member this.shutdown() =
     Async.FromContinuations <| fun (ok, _, _) ->
-      synchronizationContext.Post(SendOrPostCallback(fun o -> this.uv_close' ok),null)
+      synchronizationContext.Post(SendOrPostCallback(fun o -> this.uv_close ok),null)
       synchronizationContext.Send()
 
   interface ITransport with
     member this.read (buf : ByteSegment) =
       Async.FromContinuations <| fun (ok, _, _) ->
-        synchronizationContext.Post(SendOrPostCallback(fun o -> readOp.uv_read_start'' client buf ok),null)
+        synchronizationContext.Post(SendOrPostCallback(fun o -> readOp.uv_read_start client buf ok),null)
         synchronizationContext.Send()
 
     member this.write(buf : ByteSegment) =
       Async.FromContinuations <| fun (ok, _, _) ->
-      synchronizationContext.Post(SendOrPostCallback(fun o -> writeOp.uv_write'' client buf ok),null)
+      synchronizationContext.Post(SendOrPostCallback(fun o -> writeOp.uv_write client buf ok),null)
       synchronizationContext.Send()
 
 let createLibUvOpsPool maxOps =
@@ -367,6 +186,11 @@ let bindSocket server bindCallback=
   let r = uv_listen(server, MaxBacklog, bindCallback)
   if r<>0 then
     failwith ("Listen error: " + (new string(uv_strerror(r))))
+
+open Suave.Logging
+open Suave.Types
+open Suave.Tcp
+open Suave
 
 type LibUvSocket(pool : ConcurrentPool<OperationPair>,logger, serveClient, ip, loop , bufferManager, startData, acceptingConnections: AsyncResultCell<StartedData>) =
 
@@ -496,3 +320,8 @@ let runServerLibUv logger maxConcurrentOps bufferSize (binding: SocketBinding) s
     let! _ = Async.AwaitWaitHandle (syncEvent)
     return ()
   }
+
+type LibUvServerFactory() =
+  interface TcpServerFactory with
+    member this.create (logger,maxOps, bufferSize,binding) =
+      runServerLibUv logger maxOps bufferSize binding.socketBinding
