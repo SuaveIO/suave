501c256ab95352da3a17b66e6347c6dc4efde93b
Matthias Dittrich <matthi.d@gmail.com>
2015-12-27 11:38:21 +0100

    Use RazorEngine's internal caching; consider the current request path for resolving; reolve views in the 'Views' folder; Localize the view depending on the Accept-Language header; Add some helpers to read header values.

--- src/Suave.Razor/Library.fs
+++ src/Suave.Razor/Library.fs
@@ -10,19 +10,6 @@ open Suave.Utils.AsyncExtensions
 open RazorEngine
 
 module Razor =
-
-  let private asyncMemoize isValid f =
-    let cache = Collections.Concurrent.ConcurrentDictionary<_ , _>()
-    fun x ->
-      async {
-        match cache.TryGetValue(x) with
-        | true, res when isValid x res -> return res
-        | _ ->
-            let! res = f x
-            cache.[x] <- res
-            return res
-      }
-
   let private loadTemplate templatePath =
     async {
       let writeTime = File.GetLastWriteTime(templatePath)
@@ -32,33 +19,93 @@ module Razor =
       return writeTime, razorTemplate
     }
 
-  let loadTemplateCached = 
-    loadTemplate |> asyncMemoize (fun templatePath (lastWrite, _) -> 
-      File.GetLastWriteTime(templatePath) <= lastWrite )
-
   open RazorEngine.Configuration
   open RazorEngine.Templating
 
-  let serviceConfiguration = TemplateServiceConfiguration()
-  // generate compiled templates in memory instead of on disk as temporary files
-  serviceConfiguration.DisableTempFileLocking <- true
-  serviceConfiguration.CachingProvider <- new DefaultCachingProvider(fun t -> ())
+  let cachingProvider = new InvalidatingCachingProvider(fun t -> ())
+  let writeTimeCache = new System.Collections.Concurrent.ConcurrentDictionary<string, DateTime>()
+  let isTemplateUpdated templatePath =
+    let lastWriteTime = File.GetLastWriteTime templatePath
+    let mutable wasUpdated = false
+    writeTimeCache.AddOrUpdate(templatePath, lastWriteTime, fun key oldDate ->
+      wasUpdated <- lastWriteTime > oldDate
+      lastWriteTime) |> ignore
+    wasUpdated
+  let (@@) a b = Path.Combine(a, b)
 
-  let razorService = RazorEngineService.Create(serviceConfiguration)
+  let viewPath = "Views"
+  let resolveView r templatePath =
+   resolvePath (r.runtime.homeDirectory @@ viewPath) templatePath
+   |> Path.GetFullPath
+
 
   /// razor WebPart
   ///
   /// type Bar = { foo : string }
   ///
   /// let app : WebPart =
-  ///   url "/home" >>= razor "home.chtml" { foo = "Bar" }
+  ///   url "/home" >>= razor "home" { foo = "Bar" }
   ///
-  let razor<'a> path (model : 'a) =
+  let razor<'a> (path:string) (model : 'a) =
     fun r ->
       async {
-        let templatePath = resolvePath r.runtime.homeDirectory path
-        let! writeTime, razorTemplate = loadTemplateCached templatePath
-        let cacheKey = writeTime.Ticks.ToString() + "_" + templatePath
-        let content = razorService.RunCompile(razorTemplate, cacheKey, typeof<'a>, model)
-        return! Response.response HTTP_200 (UTF8.bytes content) r
-        }
+        let languageOptions =
+          Headers.getHeader "Accept-Language" r
+          |> Headers.getSplitValues
+          |> Headers.getWeightedValues
+          |> Seq.choose (fst >> Headers.parseCultureInfo)
+
+        let resolvePaths =
+          r.request.url.Segments
+          |> Seq.fold (fun paths item ->
+            match paths with
+            | h :: _ -> (h @@ item) :: paths
+            | _ -> [ item ]) []
+          |> Seq.toList
+
+        let serviceConfiguration = TemplateServiceConfiguration()
+        // generate compiled templates in memory instead of on disk as temporary files
+        serviceConfiguration.DisableTempFileLocking <- true
+        serviceConfiguration.CachingProvider <- cachingProvider
+        serviceConfiguration.TemplateManager <-
+          { new ITemplateManager with
+              member x.AddDynamic(_, _) = failwith "not implemented"
+              member x.Resolve key =
+                let _, readTemplate = loadTemplate key.Name |> Async.RunSynchronously
+                LoadedTemplateSource(readTemplate, key.Name) :> _
+              member x.GetKey (name, resolveType, context) =
+                let resolvedEntry =
+                  if not <| Path.IsPathRooted name then
+                    let pathWithoutExtension =
+                      Path.GetDirectoryName name @@ Path.GetFileNameWithoutExtension name
+
+                    let pathsWithExtension =
+                      languageOptions
+                      |> Seq.map (fun ci -> sprintf "%s-%s.cshtml" pathWithoutExtension ci.IetfLanguageTag)
+                      |> Seq.append [ sprintf "%s.cshtml" pathWithoutExtension ]
+
+                    resolvePaths
+                    |> Seq.collect (fun path ->
+                      pathsWithExtension
+                      |> Seq.map (fun pathWithExtension -> path @@ pathWithExtension))
+                    |> Seq.map (resolveView r)
+                    |> Seq.filter File.Exists
+                    |> Seq.tryFind (fun _ -> true)
+                    // |> Seq.tryHead // F# 4
+                  else Some name
+                match resolvedEntry with
+                | Some (resolvedPath) ->
+                  let key = NameOnlyTemplateKey(resolvedPath, resolveType, context) :> ITemplateKey
+                  if isTemplateUpdated resolvedPath then
+                    cachingProvider.InvalidateCache(key)
+                  key
+                | _ -> 
+                  failwithf "Could not resolve requested view '%s' in '%A'" name resolvePaths
+                 }
+
+        let razorService = RazorEngineService.Create(serviceConfiguration)
+        let viewBag = RazorEngine.Templating.DynamicViewBag()
+        viewBag.AddValue("HttpContext", r)
+        let content = razorService.RunCompile(path, typeof<'a>, model, viewBag)
+        return! Response.response HTTP_200 (System.Text.Encoding.UTF8.GetBytes content) r
+      }
