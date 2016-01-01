ea5e43824288ffe1f9a9069988c3da0fb3134346
Matthias Dittrich <matthi.d@gmail.com>
2016-01-01 21:14:16 +0100

    fixup some smaller bugs which prevented the view localization to work properly.

--- src/Suave.Tests/Razor.fs
+++ src/Suave.Tests/Razor.fs
@@ -30,4 +30,20 @@ let razorTest (cfg :SuaveConfig) =
         runWithConfig (razor<Foo> "razor" { bar = "test" }) |> req HttpMethod.GET "/" None)
     testCase "simple test for fallback" <| fun _ ->
       Assert.Equal("process razor file", "Hello test",
-        runWithConfig (razor<Foo> "fallback" { bar = "test" }) |> req HttpMethod.GET "/" None) ]
+        runWithConfig (razor<Foo> "fallback" { bar = "test" }) |> req HttpMethod.GET "/" None)
+    testCase "test if accept-language views work" <| fun _ ->
+      Assert.Equal("process razor file", "Localized test",
+        runWithConfig (razor<Foo> "razor" { bar = "test" })
+        |> reqResp HttpMethod.GET "/" "" None None System.Net.DecompressionMethods.None
+                   (fun r ->
+                      r.Headers.Add("Accept-Language", "de-DE")
+                      r
+                   ) contentString)
+    testCase "test if accept-language views work, even when casing is incorrect" <| fun _ ->
+      Assert.Equal("process razor file", "Localized test",
+        runWithConfig (razor<Foo> "razor" { bar = "test" })
+        |> reqResp HttpMethod.GET "/" "" None None System.Net.DecompressionMethods.None
+                   (fun r ->
+                      r.Headers.Add("Accept-Language", "de-de")
+                      r
+                   ) contentString) ]
