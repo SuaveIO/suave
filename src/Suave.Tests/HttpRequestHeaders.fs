f1da224dac387668178e3f8eb8a2de040fb72f85
kos59125 <mail@recyclebin.jp>
2016-01-13 10:22:18 +0900

    fix misspelling

--- src/Suave.Tests/HttpRequestHeaders.fs
+++ src/Suave.Tests/HttpRequestHeaders.fs
@@ -7,7 +7,7 @@ open Suave
 [<Tests>]
 let headers (_:SuaveConfig) =
   testList "Request header letter case" [
-    testCase "compare header names case-insentively" <| fun _ ->
+    testCase "compare header names case-insensitively" <| fun _ ->
       let req = { HttpRequest.empty with headers = ["x-suave-customheader", "value"] }
       let actual = req.header "X-Suave-CustomHeader"
       Assert.Equal ("results in Choise1Of2", Choice1Of2 "value", actual)
