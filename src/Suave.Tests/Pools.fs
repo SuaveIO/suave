5e33f88b5b7c1d81d77899fbe39289b95588748a
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2016-01-25 11:43:32 -0500

    unit tests

---
+++ src/Suave.Tests/Pools.fs
@@ -0,0 +1,24 @@
+﻿module Pools
+
+open Suave
+open Suave.Sockets
+open Fuchu
+
+type Foo() =
+  let foo = "foo"
+
+[<Tests>]
+let poolTests (config: SuaveConfig) =
+  testList "test pool autogrow capability" [
+
+    testCase "ConcurrentPool" <| fun _ ->
+      let pool = ConcurrentPool<Foo>()
+      for i = 0 to 10 do pool.Push (new Foo())
+      for i = 0 to 20 do pool.Pop() |> ignore
+      Assert.Equal("test ran to completion", true, true)
+
+    testCase "BufferManager" <| fun _ ->
+      // 5 buffers
+      let bufferManager = new BufferManager(2560, 512, config.logger)
+      for i = 0 to 10 do bufferManager.PopBuffer() |> ignore
+      Assert.Equal("test ran to completion", true, true)]
