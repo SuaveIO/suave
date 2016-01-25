cf7590e507cb347e32d4f6d37f50b4ddfa060bff
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2016-01-25 12:06:16 -0500

    fix unit test

--- src/Suave.Tests/Pools.fs
+++ src/Suave.Tests/Pools.fs
@@ -13,12 +13,13 @@ let poolTests (config: SuaveConfig) =
 
     testCase "ConcurrentPool" <| fun _ ->
       let pool = ConcurrentPool<Foo>()
+      pool.ObjectGenerator <- fun _ -> new Foo()
       for i = 0 to 10 do pool.Push (new Foo())
       for i = 0 to 20 do pool.Pop() |> ignore
       Assert.Equal("test ran to completion", true, true)
 
     testCase "BufferManager" <| fun _ ->
       // 5 buffers
-      let bufferManager = new BufferManager(2560, 512, config.logger)
+      let bufferManager = new BufferManager(2560, 512, config.logger, true)
       for i = 0 to 10 do bufferManager.PopBuffer() |> ignore
       Assert.Equal("test ran to completion", true, true)]
