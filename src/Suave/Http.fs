94095c2884b18eee26d987332ec11a1a1fcbf77d
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2016-02-25 02:31:01 +0000

    makes the compiler happy

--- src/Suave/Http.fs
+++ src/Suave/Http.fs
@@ -19,6 +19,8 @@ module Http =
   open Suave.Log
   open Suave.Logging
 
+  open Microsoft.FSharp.Reflection
+
   [<RequireQualifiedAccess>]
   type HttpMethod =
     | GET
@@ -196,8 +198,8 @@ module Http =
   and private HttpCodeStatics() =
     static member val mapCases : Lazy<Map<string,HttpCode>> =
       lazy
-        Microsoft.FSharp.Reflection.FSharpType.GetUnionCases(typeof<HttpCode>)
-        |> Array.map (fun case -> case.Name, Microsoft.FSharp.Reflection.FSharpValue.MakeUnion(case, [||]) :?> HttpCode)
+        FSharpType.GetUnionCases(typeof<HttpCode>)
+        |> Array.map (fun case -> case.Name, FSharpValue.MakeUnion(case, [||]) :?> HttpCode)
         |> Map.ofArray
 
   type HttpCookie =
