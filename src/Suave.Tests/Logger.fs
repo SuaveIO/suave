1931360925931a926199fd5db644c77a0cb6449d
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2016-12-22 21:54:29 -0500

    [tests] get rid of Thread.Sleep on logger test.

--- src/Suave.Tests/Logger.fs
+++ src/Suave.Tests/Logger.fs
@@ -15,8 +15,7 @@ let ``CombiningLogger.log`` (_ : SuaveConfig) =
       let log2 = InspectableLog()
       let log3 = InspectableLog()
       let logger = CombiningTarget([| "combining-target" |], [ log1; log2; log3 ]) :> Logger
-      logger.info (Message.eventX "hello")
-      System.Threading.Thread.Sleep 1000
+      logger.log LogLevel.Info (Message.eventX "hello") |> Async.RunSynchronously
       Expect.equal log1.logs.Length 1 "log1 should have 1 logged message"
       Expect.equal log2.logs.Length 1 "log2 should have 1 logged message"
       Expect.equal log3.logs.Length 1 "log3 should have 1 logged message"
