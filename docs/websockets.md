c72daad0e072f81f8a20c15d2277c45b41d6f175
Lucas Reis <lucasmreis@gmail.com>
2017-01-06 00:13:14 -0200

    [docs] WebSockets

---
+++ docs/websockets.md
@@ -0,0 +1,55 @@
+---
+layout: default
+---
+
+Realtime Messages With WebSockets
+---------------------------------
+
+It's easy to set up WebSockets with Suave.
+
+First, define a function that takes `WebSocket` and `HttpContext` typed parameters, and returns a socket computation expression:
+
+{% highlight fsharp %}
+let ws (webSocket : WebSocket) (context: HttpContext) =
+    socket {
+      ...
+    }
+{% endhighlight %}
+
+Next, use the `read` and `send` function to receive and send messages to the clients:
+
+{% highlight fsharp %}
+    socket {
+        let mutable loop = true
+
+        while loop do
+            let! msg = webSocket.read()
+
+            match msg with
+            | (Text, data, true) ->
+                let str = UTF8.toString data
+                let response = sprintf "response to %s" str
+                let byteResponse =
+                    response
+                    |> System.Text.Encoding.ASCII.GetBytes
+                    |> ByteSegment
+                do! webSocket.send Text byteResponse true
+
+            | (Close, _, _) ->
+                let emptyResponse = [||] |> ByteSegment
+                do! webSocket.send Close emptyResponse true
+                loop <- false
+
+            | _ -> ()
+      }
+{% endhighlight %}
+
+Then, use the `handShake` function to fit it in your web server:
+
+{% highlight fsharp %}
+let app : WebPart =
+    choose [
+      path "/websocket" >=> handShake ws
+      GET >=> choose [ path "/" >=> file "index.html"; browseHome ]
+      NOT_FOUND "Found no handlers." ]
+{% endhighlight %}
--- examples/WebSocket/Program.fs
+++ examples/WebSocket/Program.fs
@@ -16,29 +16,36 @@ open Suave.Sockets
 open Suave.Sockets.Control
 open Suave.WebSocket
 
-let echo (webSocket : WebSocket) =
-  fun cx -> socket {
-    let loop = ref true
-    while !loop do
-      let! msg = webSocket.read()
-      match msg with
-      | (Text, data, true) ->
-        let str = UTF8.toString data
-        do! webSocket.send Text (ArraySegment data) true
-      | (Ping, _, _) ->
-        do! webSocket.send Pong (ArraySegment([||])) true
-      | (Close, _, _) ->
-        do! webSocket.send Close (ArraySegment([||])) true
-        loop := false
-      | _ -> ()
-  }
+let ws (webSocket : WebSocket) (context: HttpContext) =
+    socket {
+        let mutable loop = true
+
+        while loop do
+            let! msg = webSocket.read()
+             
+            match msg with
+            | (Text, data, true) ->
+                let str = UTF8.toString data
+                let response = sprintf "response to %s" str
+                let byteResponse = 
+                    response 
+                    |> System.Text.Encoding.ASCII.GetBytes 
+                    |> ByteSegment
+                do! webSocket.send Text byteResponse true
+
+            | (Close, _, _) ->
+                let emptyResponse = [||] |> ByteSegment
+                do! webSocket.send Close emptyResponse true
+                loop <- false
+
+            | _ -> ()
+      }
 
 let app : WebPart =
   choose [
-    path "/websocket" >=> handShake echo
-    GET >=> choose [ path "/" >=> file "index.html"; browseHome ];
-    NOT_FOUND "Found no handlers."
-    ]
+    path "/websocket" >=> handShake ws
+    GET >=> choose [ path "/" >=> file "index.html"; browseHome ]
+    NOT_FOUND "Found no handlers." ]
 
 [<EntryPoint>]
 let main _ =
