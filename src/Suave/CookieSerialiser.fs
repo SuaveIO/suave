7ecf27c57f0497175b7d7be3f19ae9876485210e
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2017-12-28 21:14:40 -0500

    remove json cookie serializer

--- src/Suave/CookieSerialiser.fs
+++ src/Suave/CookieSerialiser.fs
@@ -1,25 +1,10 @@
-namespace Suave
+﻿namespace Suave
 
 open System.IO
 
 type CookieSerialiser =
   abstract serialise : Map<string, obj> -> byte []
   abstract deserialise : byte [] -> Map<string, obj>
-
-open System.Runtime.Serialization.Json
-
-type JsonFormatterSerialiser() =
-  interface CookieSerialiser with
-    member x.serialise m =
-      use ms = new MemoryStream()
-      let f = new DataContractJsonSerializer(typeof<_>)
-      f.WriteObject(ms, m)
-      ms.ToArray()
-
-    member x.deserialise data =
-      use ms = new MemoryStream(data)
-      let f = new DataContractJsonSerializer(typeof<_>)
-      f.ReadObject(ms) :?> _
 
 open System.Runtime.Serialization.Formatters.Binary
 
