ffb38755dfd2535b7ac64c0828f6b0d35308e13c
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2018-10-01 14:41:06 -0400

    this works

--- build.fsx
+++ build.fsx
@@ -22,7 +22,7 @@ let release = ReleaseNotes.load "RELEASE_NOTES.md"
 let Configuration = Environment.environVarOrDefault "CONFIGURATION" "Release"
 
 // Lazily install DotNet SDK in the correct version if not available
-let install = lazy DotNet.install (fun x -> { x with Version = DotNet.Version "2.1.402" })
+let install = lazy DotNet.install DotNet.Versions.Release_2_1_402
 
 // Define general properties across various commands (with arguments)
 let inline withWorkDir wd =
