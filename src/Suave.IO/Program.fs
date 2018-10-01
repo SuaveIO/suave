d081c56e245e83b7751abf7e58f98bc5a5ca68a5
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2018-10-01 13:56:50 -0400

    unused

--- src/Suave.IO/Program.fs
+++ src/Suave.IO/Program.fs
@@ -2,17 +2,11 @@
 
 open Argu
 open Suave
-open Suave.Filters
-open Suave.Redirection
-open Suave.RequestErrors
 open Suave.ServerErrors
-open Suave.Operators
-open Suave.Model
 open Suave.Logging
 open Suave.Logging.Message
 open System.IO
 open System.Net
-open Stripe
 
 let logger = Log.create "Suave.IO"
 
@@ -36,34 +30,8 @@ let addExnLogging (fwp: 'a -> WebPart) =
         return! INTERNAL_ERROR "Unhandled internal exception" ctx
     }
 
-let subscribeCustomer =
-  let callApi (email, token, tokenType, planId: string): WebPart =
-    let custs = StripeCustomerService()
-    let subs = StripeSubscriptionService()
-    fun ctx ->
-      async {
-        let opts = StripeCustomerCreateOptions()
-        opts.SourceToken <- token
-        opts.Email <- email
-        opts.Description <- "From Suave.io site"
-        opts.PlanId <- planId
-
-        let! cust = custs.CreateAsync(opts)
-        let! sub = subs.CreateAsync((cust :> StripeEntityWithId).Id, planId)
-        return! FOUND "promote-thank-you.html" ctx
-      }
-
-  bindReq (fun r -> binding {
-      let! email = r.formData "stripeEmail"
-      let! token = r.formData "stripeToken"
-      let! tokenType = r.formData "stripeToken"
-      let! planId = r.formData "stripePlanId"
-      return email, token, tokenType, planId
-  }) (addExnLogging callApi) BAD_REQUEST
-
 let app: WebPart =
   choose [
-    POST >=> path "/promote" >=> subscribeCustomer
     Files.browseHome
     Files.browseFileHome "index.html"
     request (fun r -> INTERNAL_ERROR (sprintf "No file found at path %s" r.url.AbsolutePath))
@@ -75,8 +43,6 @@ let main argv =
   let parsed = parser.Parse argv
   let (host, port) = parsed.GetResult <@ Binding @>
   let home = parsed.GetResult <@ Home @> |> Path.GetFullPath
-  ServicePointManager.SecurityProtocol <- SecurityProtocolType.Tls12
-  StripeConfiguration.SetApiKey (Env.varRequired "STRIPE_PRIVATE_KEY")
 
   printfn "Root path: %s" home
 
