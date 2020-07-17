f779fc482822380d097cccec3ab1f9b89101ee5d
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2020-07-17 16:36:52 -0400

    enhance auth basic unit test

--- src/Suave.Tests/HttpAuthentication.fs
+++ src/Suave.Tests/HttpAuthentication.fs
@@ -29,33 +29,38 @@ let authTests cfg =
     | true, username -> sprintf "hello %O" username
     | false,_ -> "no user"
 
+  let okUser =
+    context(fun ctx ->
+      printfn "%A" ctx
+      OK(getUserName ctx))
+
+  let app =
+    choose [
+      GET >=> path "/non-protected1" >=> okUser
+      GET >=> path "/protected" >=> authenticateBasic ((=) ("foo", "bar")) okUser
+      GET >=> path "/non-protected2" >=> okUser ]
+
   testList "basic authetication" [
     testCase "add username to userstate for protectedPart only" <| fun _ ->
-      let okUser = context <| fun ctx ->
-        getUserName ctx
-        |> OK
-
-      let app =
-        choose [
-          GET >=> path "/non-protected1" >=> okUser
-          GET >=> path "/protected" >=> authenticateBasic ((=) ("foo", "bar")) okUser
-          GET >=> path "/non-protected2" >=> okUser ]
 
       let req path reqMod = reqResp HttpMethod.GET path "" None None DecompressionMethods.None reqMod (fun res -> res.Content.ReadAsStringAsync().Result)
+
+      let res = runWithConfig app |> req "/non-protected1" id
+      Expect.equal res "no user" "access to /non-protected1 should result in no username"
+
+      let res = runWithConfig app |> req "/non-protected2" id
+      Expect.equal res "no user" "access to /non-protected2 should result in no username"
 
       let res = runWithConfig app |> req "/protected" (fun reqmsg -> reqmsg.Headers.Authorization <- AuthenticationHeaderValue("Basic", basicCredentials); reqmsg)
       Expect.equal res "hello foo" "should be username"
 
       let res = runWithConfig app |> req "/non-protected1" id
-      Expect.equal res "no user" "should be no username"
+      Expect.equal res "no user" "access to /non-protected1 should result in no username (2)"
 
       let res = runWithConfig app |> req "/non-protected2" id
-      Expect.equal res "no user" "should be no username"
+      Expect.equal res "no user" "access to /non-protected2 should result in no username (2)"
 
     testCase "add username to userstate for protectedPart (async)" <| fun _ ->
-      let okUser = context <| fun ctx ->
-        getUserName ctx
-        |> OK
 
       let authenticate credentials = async { return credentials = ("foo", "bar") }
       let app = GET >=> authenticateBasicAsync authenticate okUser
