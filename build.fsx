9f7d76d7c4b795f5bfb81082d23bb4d397998c3c
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2023-01-15 22:05:32 -0500

    Update build.fsx

--- build.fsx
+++ build.fsx
@@ -77,7 +77,7 @@ Target.create "Build" <| fun _ ->
 
 Target.create "Tests" <| fun _ ->
   let path = "src" </> "Suave.Tests"
-  let res = DotNet.exec id "run" (sprintf "--framework netcoreapp3.1 --project %s -- --summary --sequenced" path)
+  let res = DotNet.exec id "run" (sprintf "--framework net6.0 --project %s -- --summary --sequenced" path)
   if not res.OK then
     res.Errors |> Seq.iter (eprintfn "%s")
     failwith "Tests failed."
