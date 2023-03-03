c7ebcee60e83780f0db526fb771bf84509439a46
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2023-03-03 11:09:37 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 source .env
-dotnet tool install paket
 dotnet tool restore
 dotnet paket restore
-dotnet restore build.proj
-dotnet tool install fake-cli
-dotnet fake build $@
+dotnet run --project ./build/build.fsproj -- -t Tests
